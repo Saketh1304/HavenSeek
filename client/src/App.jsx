501991667c330565db9920fdcbb8778a49d2ced0
Your Name <you@example.com>
2024-03-14 23:48:00 +0530

    complete profile page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Home from './pages/Home'
 import { SignUp } from './pages/SignUp'
 import {SignIn} from './pages/SignIn'
-import { Profile } from './pages/Profile'
+import Profile from './pages/Profile'
 import { About } from './pages/About'
 import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
