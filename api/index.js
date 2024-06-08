2a287529209e5ce04270634bb9104b76ba3c7d98
Your Name <you@example.com>
2024-06-08 19:03:26 +0530

    complete upload listing images functionality

--- api/index.js
+++ api/index.js
@@ -1,28 +1,50 @@
-import express from "express";
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import userRouter from "./routes/user.route.js"
-import authRouter from "./routes/auth.route.js"
-import cookieParser from 'cookie-parser'
+import express from 'express';
+import dotenv from 'dotenv';
 dotenv.config();
-mongoose.connect(process.env.MONGO)
-const app=express();
+import mongoose from 'mongoose';
+
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
+import cookieParser from 'cookie-parser';
+//import path from 'path';
+
+// console.log(process.env.MONGO);
+// console.log(process.env.JWT_SECRET);
+mongoose
+  .connect('mongodb+srv://saketh123:saketh@mern-estate.6r03bnp.mongodb.net/mern-estate')
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  
+
+const app = express();
 
 app.use(express.json());
+
 app.use(cookieParser());
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
-})
 
-app.use("/api/user",userRouter);
-app.use('/api/auth',authRouter);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
 
-app.use((err,req,res,next)=>{
-    const statusCode=err.statusCode || 500;
-    const message=err.message || 'Internet Server Error';
-    return res.status(statusCode).json({
-        success:false,
-        statusCode,
-        message
-    })
-})
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.use('/api/listing',listingRouter)
+
+
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,20 +1,20 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice'; // Adjusted import statement
-import {persistReducer} from 'redux-persist'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'; // help in setting up your store
+import userReducer from './user/userSlice'; // this handles changes to the user state.Its imported from another file
+import {persistReducer} from 'redux-persist' 
 import storage from 'redux-persist/lib/storage';
-import persistStore from 'redux-persist/es/persistStore';
-const rootReducer=combineReducers({user:userReducer})
+import persistStore from 'redux-persist/es/persistStore'; // the above 3 help save and restore your redux state
+const rootReducer=combineReducers({user:userReducer}) // used to combine multiple reducers into one
 const persistConfig={
     key:'root',
     storage,
     version:1,
-}
-const persistedReducer=persistReducer(persistConfig,rootReducer)
+}  // This is a configuration object for redux-persist. It tells redux-persist to use local storage and assigns a key ('root') to identify the persisted state.
+const persistedReducer=persistReducer(persistConfig,rootReducer) // persistReducer wraps the rootReducer with the persistence logic. This means our state will be saved and restored automatically.
 export const store = configureStore({
     reducer:persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false, 
     }),
 });
-export const persistor = persistStore(store);
+export const persistor = persistStore(store); // persistStore creates a persistor object that will handle the actual saving and restoring of the state
 
