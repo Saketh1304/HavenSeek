0a7af8346192866f596c498264fe1a73b98b3cb9
Your Name <you@example.com>
2024-06-16 01:25:42 +0530

    completed the about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
-export const About = () => {
+export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About HavenSeek</h1>
+      <p className='mb-4 text-slate-700'>At HavenSeek, our mission is to help you find your ideal haven—a place where comfort, convenience, and community come together. As a premier real estate agency, we specialize in locating and securing properties that meet your unique needs and desires, whether you're looking to buy, sell, or rent.</p>
+      <p className='mb-4 text-slate-700'>
+      Our dedicated team of expert agents understands that a home is more than just a place to live; it’s a sanctuary where memories are made and dreams are realized. With a deep understanding of the local market and a personalized approach to service, we strive to make your real estate journey smooth and fulfilling.
+      </p>
+      <p className='mb-4 text-slate-700'>HavenSeek is committed to providing you with unparalleled service and expert advice at every step. Our agents bring a wealth of experience and a passion for helping clients discover properties that truly feel like home. From bustling urban neighborhoods to tranquil suburban retreats, we know how to match you with the perfect haven that fits your lifestyle.</p>
+      <p className='mb-4 text-slate-700'>We believe in the importance of creating lasting relationships with our clients. By taking the time to understand your individual preferences and goals, we ensure that every interaction is tailored to your needs. At HavenSeek, we are dedicated to making the process of finding your haven as seamless and enjoyable as possible.</p>
+      <p className='mb-4 text-slate-700'>Your journey to finding the perfect haven starts here with HavenSeek. Let us guide you to a place you’ll love to call home.</p>
+    </div>
   )
 }
