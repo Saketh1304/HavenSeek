4cd67d56c296bf630261e43bc815fdf26740c119
Your Name <you@example.com>
2024-06-16 01:59:48 +0530

    update

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-//import path from 'path';
+import path from 'path';
 
 // console.log(process.env.MONGO);
 // console.log(process.env.JWT_SECRET);
@@ -19,6 +19,9 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+  
+  const __dirname = path.resolve();
 
   
 
@@ -37,6 +40,11 @@ app.use('/api/auth', authRouter);
 
 app.use('/api/listing',listingRouter)
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 
 app.use((err, req, res, next) => {
